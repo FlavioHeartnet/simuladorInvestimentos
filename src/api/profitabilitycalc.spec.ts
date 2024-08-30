75c8a75e1d34e171764432b7a499bdf1390e6da0
Flavio Nogueira <flavionogueirabarros@gmail.com>
2024-08-30 11:59:03 -0300

    profitability, calc for aporte mensal

--- src/api/profitabilitycalc.spec.ts
+++ src/api/profitabilitycalc.spec.ts
@@ -1,10 +1,15 @@
 import { describe, expect, test } from "vitest";
-import { calculoAporteMensal } from "./profitabilitycalc";
+import { calculoAporteMensal, obterMontanteFuturoNoTempo } from "./profitabilitycalc";
 
 describe('test profitability', ()=> {
     test('Deve receber rendimento, juros anual e periodo em anos e retornar aporte mensal ', () => {
-        const aporte = calculoAporteMensal(1763.55,13,20);
-        console.log(aporte.toFixed(2));
-        expect(aporte).not.toBe(0);
+        const aporte = calculoAporteMensal(2271.20,13,20);
+        console.log(aporte);
+        expect(aporte).toEqual(170.07);
+    });
+    test('Deve receber aporte mensal, juros anual e periodo em anos e retornar montante futuro ', () => {
+        const aporte = obterMontanteFuturoNoTempo(185,13,20);
+        console.log(aporte);
+        expect(aporte).not.toBe(0)
     });
 });
--- src/api/profitabilitycalc.ts
+++ src/api/profitabilitycalc.ts
@@ -1,9 +1,18 @@
 export function calculoAporteMensal(rendimentomensal: number, taxaJurosAnual: number, periodoAnos: number){
-    const taxaJurosMensal = taxaJurosAnual/100 / 12;
-    const montanteFuturo = (rendimentomensal/taxaJurosMensal).toFixed(2);
-    console.log('montante: '+ montanteFuturo)
-    const depositomensal = (parseFloat(montanteFuturo) * taxaJurosMensal) / Math.pow(1 + taxaJurosMensal,getMeses(periodoAnos).length)-1;
-    return depositomensal;
+    const taxaJurosMensal = (taxaJurosAnual/100) / 12;
+    const montanteFuturo = rendimentomensal/taxaJurosMensal;
+    console.log('montante: '+ montanteFuturo.toFixed(2));
+    const periodoMeses = getMeses(periodoAnos).length;
+    // ! Esta formula esta dando um valor menor do que o serio realmente o necessario para o montante futuro
+    const depositomensal = rendimentomensal / ((1 + taxaJurosMensal)**periodoMeses)-1 ;//formula de aporte futuro FV*i/(1+i)**n - 1
+    return parseFloat(depositomensal.toFixed(2));
+}
+
+export function obterMontanteFuturoNoTempo(aporteMensal: number, taxaJurosAnual: number, periodoAnos: number){
+  const taxaJurosMensal = (taxaJurosAnual/100) / 12;
+  const periodoMeses = getMeses(periodoAnos).length;
+  const montanteFuturo = aporteMensal * ((1+taxaJurosMensal)**periodoMeses - 1)/taxaJurosMensal;
+  return parseFloat(montanteFuturo.toFixed(2));
 }
 
 export function getMeses(periodoAnos:number){
